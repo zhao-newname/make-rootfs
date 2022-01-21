1cc21cd4e0f71fd4d84961bf06de9b9bd37ca158
Jeff Zhao <jeff.zhao@nebula-matrix.com>
2022-01-20 18:07:27 -0800

    add gdb with stop breakpoint

--- start_qemu/start_qemu.sh
+++ start_qemu/start_qemu.sh
@@ -1,4 +1,5 @@
-qemu-system-x86_64 -kernel /home/jeff/workstation/kernel_src/linux-hwe-5.11-5.11.0/arch/x86_64/boot/bzImage \
+qemu-system-x86_64 -s \
+-kernel /home/jeff/workstation/kernel_src/linux-hwe-5.11-5.11.0/arch/x86_64/boot/bzImage \
 -boot c -m 2049M \
 -device e1000,netdev=brdev0 \
 -netdev bridge,id=brdev0,br=virbr0 \
